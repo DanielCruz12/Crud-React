2bf8fd1a0f1ef9e1f98e16cacede4728e51c2747
daniel <danielduboxd@gmail.com>
2022-07-05 16:08:59 -0600

    new branch

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css'
 import { TasksCreator } from "./components/TasksCreator";
 
 function App() {
-  const [listarTareas, setListarTareas] = useState([
-    { name: "new1" },
-    { name: "new2" },
-    { name: "new3" },
-  ]);
-
-  //si no existe, se puede agregar
-  const crearTareas = (nombreTarea) => {
-    if (!listarTareas.find((task) => task.name === nombreTarea))
-      setListarTareas([...listarTareas, { name: nombreTarea }]);
-  };
+  
   return (
     <div className="App">
-      <TasksCreator crearTareas={crearTareas}></TasksCreator>
-
-      <table>
-        <thead>
-          <tr>
-            <th>tasks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listarTareas.map((tareas) => (
-            <tr key={tareas.name}>
-              <td>{tareas.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <TasksCreator></TasksCreator>
     </div>
   );
 }
--- src/components/TasksCreator.js
+++ src/components/TasksCreator.js
@@ -1,25 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {Table} from './Table'
 
-export const TasksCreator = ({crearTareas}) => {
+export const TasksCreator = () => {
   const [nuevoNombreTarea, setNuevoNombreTarea] = useState("");
+
+  const [listarTareas, setListarTareas] = useState([]);
+
+  //si no existe, se puede agregar
+  const crearTareas = (nombreTarea) => {
+    if (!listarTareas.find((task) => task.name === nombreTarea))
+      setListarTareas([...listarTareas, { name: nombreTarea,  done: false }]);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     crearTareas(nuevoNombreTarea);
-    localStorage.setItem("task", nuevoNombreTarea);
     setNuevoNombreTarea("");
   };
 
+  const toggleTask = (task) => {
+    setListarTareas(listarTareas.map(t => (t.name === task.name) ? {...t, done: !t.done} : t))
+  }
+  
+  useEffect(() => {
+    let data = localStorage.getItem("tareas");
+    if (data) {
+      setListarTareas(JSON.parse(data));
+    }
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(listarTareas));
+  }, [listarTareas]);
+
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="write a task"
-          value={nuevoNombreTarea}
-          onChange={(e) => setNuevoNombreTarea(e.target.value)}
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-    </>
+   <div className="App">
+      <div >
+
+      </div>
+      
+      <Table listarTareas={listarTareas} toggleTask={toggleTask} ></Table>
+    </div>
   );
 };
+
+
